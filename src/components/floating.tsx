cd17448f4ce0ceb0218a196e9c968f3f43d0edfd
Arjun Aditya <arjunaditya92006@gmail.com>
2026-02-17 15:02:06 +0530

    Update floating.tsx

--- src/components/floating.tsx
+++ src/components/floating.tsx
@@ -1,7 +1,7 @@
 export function Floating() {
   return (
     <div>
-      <div className="fixed bottom-4 bg-white border border-neutral-200 rounded-lg px-2 py-1 right-4 z-50 flex flex-col gap-2">
+      <div className="fixed bottom-8 bg-white border border-neutral-200 rounded-lg px-2 py-1 right-4 z-50 flex flex-col gap-2">
         Beta & Work in Progress
       </div>
     </div>
